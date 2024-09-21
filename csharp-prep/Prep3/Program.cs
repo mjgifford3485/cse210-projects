cb6f09704e53cd109608c91e4ecb64a43868ddde
mjgifford3485 <mjgifford3485@gmail.com>
2024-09-21 16:44:15 -0700

    created third prep program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+        
+
+        while (guess != number)
+        {
+            Console.Write("Please enter your guess: ");
+            string userInput = Console.ReadLine();
+            guess = int.Parse(userInput);
+            if (guess < number)
+            {
+             Console.WriteLine("Higher");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else 
+            {
+                Console.WriteLine("Correct");
+            }
+        }
     }
 }
