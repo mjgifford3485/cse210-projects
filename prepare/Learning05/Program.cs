c695a6397ea4a146bf512223442adca9bb2e3fa2
mjgifford3485 <mjgifford3485@gmail.com>
2024-10-17 13:34:03 -0700

    week 5 learning probject

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        MathAssignment mathAssignment = new MathAssignment("Matthew", "Math", "3", "1-15");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Matthew", "Writing", "Creative Writing");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
