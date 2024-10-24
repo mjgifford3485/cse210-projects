54296908f5630b8663000eb2eee0f89282c485f0
mjgifford3485 <mjgifford3485@gmail.com>
2024-10-23 17:20:02 -0700

    Finished learning 06 program

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+        Square square = new Square("Orange", 10);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("Blue", 10, 5);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("Purple", 25);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color =shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of: {area}");
+        }
     }
 }
