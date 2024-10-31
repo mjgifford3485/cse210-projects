8b076e558e2d76931ebb8265e2dbda9863744703
mjgifford3485 <mjgifford3485@gmail.com>
2024-10-31 10:00:22 -0700

    dded comment

--- prove/Develop06/Program.cs
+++ prove/Develop06/Program.cs
@@ -2,6 +2,7 @@ using System;
 
 class Program
 {
+    // for creativity, created a class that updates the player's level and title depending on how many points they've earned
     static void Main(string[] args)
     {
         GoalManager goalManager = new GoalManager();
